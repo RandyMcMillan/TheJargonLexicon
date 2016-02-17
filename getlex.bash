753a16cf1e6ab26d5ca960e8499665b4517460ab
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-02-17 02:27:13 -0500

    renamed:    getFileByURL.bash -> getlex.bash

--- getlex.bash
+++ getlex.bash
@@ -20,3 +20,6 @@ tail -r -n ${#myarray[@]}  $PWD/b.txt > $PWD/c.txt
 # delete the first 1 lines of a file
 sed '1,4d'  $PWD/c.txt > $PWD/d.txt
 tail -r -n ${#myarray[@]}  $PWD/d.txt > $PWD/lexicon.text
+
+rm  $PWD/*.html
+rm  $PWD/*.txt
