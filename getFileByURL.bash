284936987e19489bd2780af6dcdee805f6fb9975
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-02-17 02:23:36 -0500

    modified:   getFileByURL.bash

--- getFileByURL.bash
+++ getFileByURL.bash
@@ -2,9 +2,10 @@
 rm  $PWD/*.html
 rm  $PWD/*.txt
 wget http://www.catb.org/jargon/html/go01.html
-cat $PWD/go01.html | pup 'a text{}' > lexicon.txt
+cat $PWD/go01.html | pup 'a text{}' > $PWD/a.txt
+
  # delete the first 1 lines of a file
-sed '1,4d'  $PWD/lexicon.txt > $PWD/newLexicon.txt
+sed '1,4d'  $PWD/a.txt > $PWD/b.txt
 
 declare -a myarray
 # Load file into array.
@@ -12,38 +13,10 @@ let i=0
 while IFS=$'\n' read -r line_data; do
     myarray[i]="${line_data}"
     ((++i))
-done < $PWD/newLexicon.txt
+done < $PWD/b.txt
 
-# Explicitly report array content.
-let i=0
-while (( ${#myarray[@]} > i )); do
-    #printf "${myarray[i++]}\n"
-    echo "${myarray[i++]}" >> new.txt
-done
+tail -r -n ${#myarray[@]}  $PWD/b.txt > $PWD/c.txt
 
-
-sed '1,4d'  $PWD/newLexicon.txt > $PWD/newLexicon.txt
-
-declare -a myarray2
-# Load file into array.
-let i=0
-while IFS=$'\n' read -r line_data; do
-    myarray2[i]="${line_data}"
-    ((++i))
-done < $PWD/new.txt
-
-
-
-# Explicitly report array content.
-let i=0
-while (( ${#myarray2[@]} > i )); do
-    #printf "${myarray[i++]}\n"
-    echo "${myarray2[i++]}" >> new.txt
-done
-
-for ((i = ${#myarray2[@]} - 1;i >= 0;i--)); do
-    echo ${myarray2[i]}
-done
-
-
-
+# delete the first 1 lines of a file
+sed '1,4d'  $PWD/c.txt > $PWD/d.txt
+tail -r -n ${#myarray[@]}  $PWD/d.txt > $PWD/lexicon.text
